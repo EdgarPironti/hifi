d270d4be71960fab0ae4860a622fa86552212d4a
EdgarPironti <pacepiro@hotmail.it>
2015-12-18 18:50:13 -0800

    Cash first steps

--- examples/vrShop/cash/shopCashEntityScript.js
+++ examples/vrShop/cash/shopCashEntityScript.js
@@ -4,7 +4,13 @@
     var _this;
     
     var AGENT_PLAYBACK_CHANNEL = "playbackChannel";
-    var isPlaying = false;
+    var PLAY_MESSAGE = "Play";
+    var REGISTER_NAME = "EdgarRegister"; // FIXME: Change this with the actual name
+    var CARD_ANGULAR_VELOCITY = {x:0, y:2, z:0};
+    var SCRIPT_URL = Script.resolvePath("shopCreditCardEntityScript.js");
+    
+    var cashRegisterID = null;
+    var cardID = null;
     
 
     function CashZone() {
@@ -19,21 +25,57 @@
 
         enterEntity: function (entityID) {
             print("entering in the cash area");
-            if(!isPlaying) {
-                Messages.sendMessage(AGENT_PLAYBACK_CHANNEL, "Play");
-                print("Play sent.");
-                isPlaying = true;
-            }             
+            Messages.sendMessage(AGENT_PLAYBACK_CHANNEL, PLAY_MESSAGE);
+            print("Play sent.");
+            
+            // find register
+            // var entitiesInZone = Entities.findEntitiesInBox(Entities.getEntityProperties(entityID).position, Entities.getEntityProperties(entityID).dimensions); 
+            // Vec3.print("Dimension: ", Entities.getEntityProperties(entityID).dimensions);
+            // Vec3.print("Position: ", Entities.getEntityProperties(entityID).position);
+            // Vec3.print("AvPos: ", MyAvatar.position);
+            
+            var entitiesInZone = Entities.findEntities(Entities.getEntityProperties(entityID).position, (Entities.getEntityProperties(entityID).dimensions.x)/2); 
+            entitiesInZone.forEach( function(e) { 
+                print("Found entity " + Entities.getEntityProperties(e).name);
+                if (Entities.getEntityProperties(e).name == REGISTER_NAME) {
+                    cashRegisterID = Entities.getEntityProperties(e).id;
+                    print(cashRegisterID);
+                }
+            });
+             
+            // create card on register position
+            
+            var cardPosition = Vec3.sum(Entities.getEntityProperties(cashRegisterID).position, {x : 0, y : 0.2, z : 0});
+            
+            cardID = Entities.addEntity({
+                type: "Box",
+                name: "CreditCard",
+                position: cardPosition,
+                dimensions: {x : 0.30, y : 0.12, z : 0.12},
+                collisionsWillMove: false,
+                ignoreForCollisions: false,
+                angularVelocity: CARD_ANGULAR_VELOCITY,
+                angularDamping: 0,
+                script: Script.resolvePath(SCRIPT_URL),      
+                userData: JSON.stringify({
+                    ownerKey: {
+                        ownerID: MyAvatar.sessionUUID
+                    }
+                })
+                // modelURL: "https://dl.dropboxusercontent.com/u/14127429/FBX/VRshop/shoe4.fbx",
+                // shapeType: "box"
+                // We have to put the ownerID in the card, and check that when grabbing the card. Otherwise it cannot be grabbed
+            });
+            
+            Entities.callEntityMethod(cashRegisterID, 'cashRegisterOn', null);
         },
 
         leaveEntity: function (entityID) {
-            print("leaving cash area");
-            if(isPlaying) {
-                Messages.sendMessage(AGENT_PLAYBACK_CHANNEL, "Pause");
-                print("Pause sent.");
-                isPlaying = false;
-            }
+            // destroy card
+            Entities.deleteEntity(cardID);
+            cardID = null;
             
+            Entities.callEntityMethod(cashRegisterID, 'cashRegisterOff', null);
         },
 
         unload: function (entityID) {
--- examples/vrShop/cash/shopCashierAC.js
+++ examples/vrShop/cash/shopCashierAC.js
@@ -1,31 +1,18 @@
-
 // Set the following variables to the values needed
-var clip_url = "atp:4417bd30c34b970db276ee500a16ff3677ff766a45df0cc0549f3281517190b7.hfr"; // This url is working in VRshop
-// atp:4147173bdb79d0f48d8fdec2ad3a27926177e8f67e8c1078a841e389145a52d3.hfr
+var clip_url = "atp:4147173bdb79d0f48d8fdec2ad3a27926177e8f67e8c1078a841e389145a52d3.hfr"; // This url is working in VRshop
+
+
 var PLAYBACK_CHANNEL = "playbackChannel";
-var playFromCurrentLocation = true;
+var playFromCurrentLocation = false;
 var useDisplayName = true;
 var useAttachments = true;
 var useAvatarModel = true;
-
-// Set position/orientation/scale here if playFromCurrentLocation is true
-Avatar.position = { x:0, y: 0, z: 0 };
-Avatar.orientation = Quat.fromPitchYawRollDegrees(0, 0, 0);
-Avatar.scale = 1.0;
 
 var totalTime = 0;
 var subscribed = false;
 var WAIT_FOR_AUDIO_MIXER = 1;
 
-// Script. DO NOT MODIFY BEYOND THIS LINE.
 var PLAY = "Play";
-var PAUSE = "Pause";
-
-Recording.setPlayFromCurrentLocation(playFromCurrentLocation);
-Recording.setPlayerUseDisplayName(useDisplayName);
-Recording.setPlayerUseAttachments(useAttachments);
-Recording.setPlayerUseHeadModel(false);
-Recording.setPlayerUseSkeletonModel(useAvatarModel);
 
 function getAction(channel, message, senderID) {    
     if(subscribed) {
@@ -34,31 +21,16 @@ function getAction(channel, message, senderID) {
         switch(message) {
             case PLAY:
                 print("Play");
-                if (!Agent.isAvatar) {
-                    Agent.isAvatar = true;
-                }
                 if (!Recording.isPlaying()) {
+                    Recording.setPlayerTime(0.0);
                     Recording.startPlaying();
                 }
-                Recording.setPlayerLoop(true);
-                break;
-                
-            case PAUSE:
-                print("Pause");
-                if (Recording.isPlaying()) {
-                    Recording.stopPlaying();
-                }
-                //Agent.isAvatar = false;
                 break;
                 
             default:
                 print("Unknown action: " + action);
                 break;
         }
-
-        if (Recording.isPlaying()) {
-            Recording.play();
-        }  
     }
 }
 
@@ -68,11 +40,17 @@ function update(deltaTime) {
     totalTime += deltaTime;
 
     if (totalTime > WAIT_FOR_AUDIO_MIXER) {
-        if (!subscribed) {
+        if (!subscribed) {            
             Messages.subscribe(PLAYBACK_CHANNEL);
             subscribed = true;
             Recording.loadRecording(clip_url);
-            print("I'm the agent and I am ready to receive!");
+            Recording.setPlayFromCurrentLocation(playFromCurrentLocation);
+            Recording.setPlayerUseDisplayName(useDisplayName);
+            Recording.setPlayerUseAttachments(useAttachments);
+            Recording.setPlayerUseHeadModel(false);
+            Recording.setPlayerUseSkeletonModel(useAvatarModel);
+            Agent.isAvatar = true;
+            print("------------------        I'm the agent and I am ready to receive!");
         }
     }
 
