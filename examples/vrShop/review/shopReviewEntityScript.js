d9cde02dc48b4fc1a9c0d7f761403b22abd81621
AlessandroSigna <alesigna92@gmail.com>
2016-01-09 17:01:50 -0800

    overlay during all the review phase - recording missing

--- examples/vrShop/review/shopReviewEntityScript.js
+++ examples/vrShop/review/shopReviewEntityScript.js
@@ -9,7 +9,11 @@
     var POINTER_ICON_URL = "https://dl.dropboxusercontent.com/u/14127429/FBX/VRshop/Pointer.png";
     var STAR_ON_URL = "https://dl.dropboxusercontent.com/u/14127429/FBX/VRshop/SingleStar_Yellow.png";
     var STAR_OFF_URL = "https://dl.dropboxusercontent.com/u/14127429/FBX/VRshop/SingleStar_Black.png";
+    // var STAR_ON_URL = "https://dl.dropboxusercontent.com/u/14127429/FBX/VRshop/3Star.png";
+    // var STAR_OFF_URL = "https://dl.dropboxusercontent.com/u/14127429/FBX/VRshop/1Star.png";
     var ANCHOR_ENTITY_FOR_UI_NAME = "anchorEntityForReviewUI";
+    var START_RECORDING_TEXT = "Press the bumper to start recording the review";
+    var STOP_RECORDING_TEXT = "Press the bumper to stop recording the review";
     
     
     var RIGHT_HAND = 1;
@@ -28,9 +32,12 @@
     var itemToReview = null;
     var dataBaseID = null;
     var anchorEntityForUI = null;
+    var cameraEntity = null;
     var scoreAssigned = null;
     var hoveredButton = null;
     var hoveredButtonIndex = -1;
+    var recording = false;
+    var workDone = false;
 
     var PENETRATION_THRESHOLD = 0.2;
 
@@ -43,7 +50,8 @@
     
     var mainPanel = null;
     var buttons = [];
-    var tryOnAvatarButton = null;
+    var OnAirOverlay = null;
+    var instructionsOverlay = null;
     
     
     var pointer = new Image3DOverlay({          //maybe we want to use one pointer for each hand ?
@@ -125,36 +133,59 @@
             //detect the bumper event
             var bumperPressed = Controller.getValue(this.bumper);
             if (bumperPressed && this != workingHand) {
+                //mantain active one ray at a time
                 workingHand.clean();
                 workingHand = this;
             } else if (this != workingHand) {
                 return;
             }
             
-            this.updateRay();
-            
-            //manage event on UI
-            var lastHoveredButton = hoveredButton;
-            hoveredButton = OverlayManager.findOnRay(this.pickRay);
-            print("hovered button: " + hoveredButton);
-            if (lastHoveredButton != hoveredButton) {
-                hoveredButtonIndex = -1;
-                if (hoveredButton) {
-                    for (var i = 0; i < buttons.length; i++) {
-                        if (buttons[i] == hoveredButton) {
-                            //print("Adapting overlay rendering");
-                            hoveredButtonIndex = i;
+            if (!scoreAssigned) {
+                this.updateRay();
+                
+                //manage event on UI
+                var lastHoveredButton = hoveredButton;
+                hoveredButton = OverlayManager.findOnRay(this.pickRay);
+                //print("hovered button: " + hoveredButton);
+                if (lastHoveredButton != hoveredButton) {
+                    hoveredButtonIndex = -1;
+                    if (hoveredButton) {
+                        for (var i = 0; i < buttons.length; i++) {
+                            if (buttons[i] == hoveredButton) {
+                                //print("Adapting overlay rendering");
+                                hoveredButtonIndex = i;
+                            }
                         }
                     }
+                    adaptOverlayOnHover(hoveredButtonIndex);
                 }
-                adaptOverlayOnHover(hoveredButtonIndex);
+            } else if (!instructionsOverlay.visible) {
+                workingHand.clean();
+                for (var i = 0; i < buttons.length; i++) {
+                    buttons[i].destroy();
+                }
+                buttons = [];
+                instructionsOverlay.visible = true;
             }
             
-            if (bumperPressed && !this.waitingForBumpReleased && hoveredButton) {
+            
+            if (bumperPressed && !this.waitingForBumpReleased) {
                 this.waitingForBumpReleased = true;
                 
-                scoreAssigned = hoveredButtonIndex + 1;
-                print("scoreAssigned: " + scoreAssigned);
+                if (hoveredButton) {
+                    scoreAssigned = hoveredButtonIndex + 1;
+                    print("********** scoreAssigned: " + scoreAssigned);
+                    hoveredButton = null;
+                } else if (scoreAssigned && !recording) {
+                    instructionsOverlay.text = STOP_RECORDING_TEXT;
+                    print("************ start recording");
+                    recording = true;
+                } else if (scoreAssigned && recording) {
+                    recording = false;
+                    workDone = true;
+                    _this.cleanUI();
+                    print("********** stop recording");
+                }
                     
                     // if (playButton == triggeredButton) {
                         // if(avatarEntity != null) {
@@ -175,25 +206,35 @@
         },
         
         this.clean = function() {
+            print("hand clean");
             this.pickRay = null;
-            this.overlayLine.destroy();
+            if (this.overlayLine) {
+                this.overlayLine.destroy();
+            }
             this.overlayLine = null;
             pointer.visible = false;
         }
     };
     
     function update(deltaTime) {
-        //update the rays from both hands
-        leftController.updateHand();
-        rightController.updateHand();
-        if (!itemToReview && isUIWorking) {
-            //getting here when the inspect phase end, so we want to clean the UI
+        
+        if (!workDone) {
+            leftController.updateHand();
+            rightController.updateHand();
+        } else {
+            _this.cleanUI();
+            print("******** WORK DONE");
+            Script.update.disconnect(update);
+        }
+        
+        
+        if (!insideZone && isUIWorking) {
             // Destroy rays
             _this.cleanUI();
             
-            // Destroy overlay
-            mainPanel.destroy();
-            isUIWorking = false;
+            // // Destroy overlay
+            // mainPanel.destroy();
+            // isUIWorking = false;
         }
         
     };
@@ -202,14 +243,12 @@
         // Find items in the zone
         var entitiesInZone = Entities.findEntities(Entities.getEntityProperties(entityID).position, (Entities.getEntityProperties(entityID).dimensions.x)/2); 
         for (var i = 0; i < entitiesInZone.length && itemToReview == null; i++) {
-            print(Entities.getEntityProperties(entitiesInZone[i]).name);
-            print(Entities.getEntityProperties(entitiesInZone[i]).userData);
+            // print(Entities.getEntityProperties(entitiesInZone[i]).name);
+            // print(Entities.getEntityProperties(entitiesInZone[i]).userData);
             
             var ownerObj = getEntityCustomData('ownerKey', Entities.getEntityProperties(entitiesInZone[i]).id, null);
             
-            if (ownerObj == null) {
-                print("No items to review.");
-            } else {
+            if (ownerObj != null) {
                 print("Not sure if review. Check " + MyAvatar.sessionUUID);
                 if (ownerObj.ownerID === MyAvatar.sessionUUID) {
                     // store item name
@@ -247,8 +286,6 @@
             if (Entities.getEntityProperties(entitiesInZone[i]).name == databaseEntityName) {
                 dataBaseID = entitiesInZone[i];
                 print("Database found! " + entitiesInZone[i]);
-            } else {
-                print("No database for this item.");
             }
         }
         
@@ -276,6 +313,7 @@
         
         enterEntity: function (entityID) {
             print("entering in the review area");
+            insideZone = true;
             
             findItemToReview(entityID); //assign itemToReview
             if (itemToReview != null) {
@@ -284,21 +322,33 @@
                     findAnchorEntityForUI(entityID);
                     if (anchorEntityForUI) {
                         _this.createReviewUI();
+                        rightController = new MyController(RIGHT_HAND);     //rightController and leftController are two objects
+                        leftController = new MyController(LEFT_HAND);
+                        workingHand = rightController;
+                        print("controllers connected");
+                        Script.update.connect(update);
+                        print("update connected");
                     }
-                    rightController = new MyController(RIGHT_HAND);     //rightController and leftController are two objects
-                    leftController = new MyController(LEFT_HAND);
-                    workingHand = rightController;
-                    Script.update.connect(update);
                 }
             }
         },
 
         leaveEntity: function (entityID) {
             print("leaving the review area");
-            Script.update.disconnect(update);
+            if (!workDone) {
+                Script.update.disconnect(update);
+            }
             itemToReview = null;
             dataBaseID = null;
+            anchorEntityForUI = null;
+            scoreAssigned = null;
+            hoveredButton = null;
+            hoveredButtonIndex = -1;
+            recording = false;
+            workDone = false;
+            insideZone = false;
             _this.cleanUI();
+            print("cleaning after leaving");
         },
         
         createReviewUI : function() {
@@ -323,7 +373,7 @@
             
             for (var i = 0; i < 3; i++) {
                 buttons[i] = new Image3DOverlay({
-                    url: STAR_ON_URL,
+                    url: STAR_OFF_URL,
                     dimensions: {
                         x: 0.15,
                         y: 0.15
@@ -342,6 +392,30 @@
                 mainPanel.addChild(buttons[i]);
             }
             
+            instructionsOverlay= new Text3DOverlay({
+                    text: START_RECORDING_TEXT,
+                    isFacingAvatar: false,
+                    alpha: 1.0,
+                    ignoreRayIntersection: true,
+                    offsetPosition: {
+                        x: -0.3,
+                        y: 0,
+                        z: 0
+                    },
+                    dimensions: { x: 0, y: 0 },
+                    backgroundColor: { red: 0, green: 255, blue: 0 },
+                    color: { red: 0, green: 0, blue: 0 },
+                    topMargin: 0.00625,
+                    leftMargin: 0.00625,
+                    bottomMargin: 0.1,
+                    rightMargin: 0.00625,
+                    lineHeight: 0.06,
+                    alpha: 1,
+                    backgroundAlpha: 0.3,
+                    visible: false
+                });
+                
+            mainPanel.addChild(instructionsOverlay);
             // var aggregateScore = new Image3DOverlay({
                 // url: "https://dl.dropboxusercontent.com/u/14127429/FBX/VRshop/2Star.png",
                 // dimensions: {
@@ -380,29 +454,6 @@
             
             // mainPanel.addChild(playButton);
             
-            // var textReviewerName = new Text3DOverlay({
-                    // text: "Customer zero",
-                    // isFacingAvatar: false,
-                    // alpha: 1.0,
-                    // ignoreRayIntersection: true,
-                    // offsetPosition: {
-                        // x: 0.23,
-                        // y: 0.31,
-                        // z: 0
-                    // },
-                    // dimensions: { x: 0, y: 0 },
-                    // backgroundColor: { red: 255, green: 255, blue: 255 },
-                    // color: { red: 0, green: 0, blue: 0 },
-                    // topMargin: 0.00625,
-                    // leftMargin: 0.00625,
-                    // bottomMargin: 0.1,
-                    // rightMargin: 0.00625,
-                    // lineHeight: 0.02,
-                    // alpha: 1,
-                    // backgroundAlpha: 0.3
-                // });
-                
-            // mainPanel.addChild(textReviewerName);
             
             // reviewerScore = new Image3DOverlay({
                 // url: "https://dl.dropboxusercontent.com/u/14127429/FBX/VRshop/1Star.png",
@@ -563,8 +614,12 @@
         },
         
         cleanUI: function () {
+            print("massive clean");
             workingHand.clean();
-            mainPanel.destroy();
+            if (mainPanel) {
+                mainPanel.destroy();
+            }
+            mainPanel = null;
             isUIWorking = false;
         },
 
